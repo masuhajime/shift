90207ec54cb6a2be3960026273f7cccd58484863
masuhajime <masuhajime@gmail.com>
2019-09-22 00:27:14 +0900

    1st commit

---
+++ components/Your.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface User {
+  name: String;
+}
+
+export default Vue.extend({
+  props: {
+    user: {
+      type: Object,
+      required: true
+    } as PropOptions<User>
+  },
+  data () {
+    const message: string = 'This is a message'
+
+    return {
+      message
+    }
+  }
+})
+</script>
